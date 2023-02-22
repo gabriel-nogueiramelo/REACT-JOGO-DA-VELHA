89d06962d9da32d712be7e6a12439238e7f4670c
gabriel-nogueiramelo <109678985+gabriel-nogueiramelo@users.noreply.github.com>
2023-02-22 14:28:47 -0300

    criado logica do game

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,21 +1,46 @@
 import { useState } from 'react'
 import styles from './Game.module.css'
 
+import Icon from '../icons/Icon'
 import GameOption from '../gameOption/GameOption'
 
 function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0))
+  const [currentPlayer, setCurrentPlayer] = useState(1) 
+
+  const handleClick = (pos) => {
+
+    if (gameState[pos] === 0) {
+    let newGameState = [...gameState] 
+    newGameState[pos] = currentPlayer
+    setCurrentPlayer(currentPlayer * -1)
+    setGameState(newGameState)
+    }
+  }
 
   return (
-     <div className={styles.game}>
-      {
-        gameState.map((value, pos) => 
-        <GameOption 
-          key={`game-option-pos-${pos}`}
-          status={value}
-        />)
-      }
-     </div>
+    <div className={styles.gameContent}>
+      <div className={styles.game}>
+       {
+          gameState.map((value, pos) => 
+            <GameOption 
+              key={`game-option-pos-${pos}`}
+              status={value}
+              onClick={() => handleClick(pos)}
+            />
+           )
+         }
+       </div>
+       <div className={styles.gameInfo}>
+        <h4>Próximo a Jogar:</h4>
+        {
+          currentPlayer === 1 && <Icon iconName= "Circle" />
+        }
+        {
+          currentPlayer === -1 && <Icon iconName= "x" />
+        }
+      </div>
+    </div> 
   ) 
 }
 
